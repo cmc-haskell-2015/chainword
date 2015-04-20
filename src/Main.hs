0b61a3e41d7ea6e8f99ba3f30fab1b36f64cd400
Oleg <s.oleg@rocketmail.com>
2015-04-20 19:38:22 +0300

    something to start

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,25 @@
-module Main where
+import Data.Map
+import Data.Set
+import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.Pure.Game
 
-main :: IO ()
-main = undefined
+
+
+answ= ["kot","tort","tok"]
+
+
+main = do 
+    print "type number"
+    n <- getLine
+    print "type answer"
+    an <- getLine
+    print ((answ !! read n)== an)
+
+chEq::[Char]->[Char]->Bool
+chEq [] [] = True
+chEq (x:xs) (y:ys)| x==y = chEq xs ys
+			      | otherwise = False
+
+
+
