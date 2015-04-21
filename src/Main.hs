6d29d4dc8e8f758e55fca1db0b60ce8992c086b4
Oleg <s.oleg@rocketmail.com>
2015-04-21 10:30:35 +0300

    first results

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,54 @@
-import Data.Map
-import Data.Set
+--import Data.Map
+--import Data.Set
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.Pure.Game
+import System.IO
+import System.IO.Unsafe
 
 
 
-answ= ["kot","tort","tok"]
 
+
+answPath= "answers.txt"
+questPath= "questions.txt"
+
+fileToStr:: String -> String
+fileToStr s  = unsafePerformIO (hGetContents (unsafePerformIO (openFile s ReadMode)))
+
+find:: (Eq a) => [a] -> a -> Int
+find xs e 
+    | elem e xs = ffind xs e 
+    | otherwise = length xs 
+    where 
+        ffind (x:xs) e
+                | x == e    = 0
+                | otherwise     = 1 + ffind xs e
+
+
+parseInput:: String -> [String]
+parseInput s
+    | null s = [] 
+    | otherwise= (take (find s '\n') s): (parseInput (drop ((find s '\n')+1) s))
+
+answList= (parseInput (fileToStr answPath))
+questList= (parseInput (fileToStr questPath))
 
 main = do 
-    print "type number"
+    --print (parseInput (fileToStr answPath))
+    print "type number->"
     n <- getLine
-    print "type answer"
+    print (questList !! read n)
+    print (show (length (answList !! read n))++ "  letters")
+    print "type answer->"
     an <- getLine
-    print ((answ !! read n)== an)
+    print ((answList !! read n)== an)
+
+
+
+
+
+
 
 chEq::[Char]->[Char]->Bool
 chEq [] [] = True
---
+++ src/test.hs
@@ -0,0 +1,10 @@
+import Graphics.Gloss
+main = do
+play (InWindow "Hello World" (400, 400) (0, 0)) 
+green 
+10
+
+--picture	= Translate (-170) (-20)   -- shift the text to the middle of the window
+--		  $ Scale 0.5 0.5		   -- display it half the original size
+--		  $ Text "Hello World"	   -- text to display
+
